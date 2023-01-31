4a490d9fa6c9dab13619ef881a7f8070888f9d3d
timsueberkrueb <dev@timsueberkrueb.io>
2023-01-31 18:47:41 +0000

    Deploying to gh-pages from @ timsueberkrueb/msc-impl@f03fd2bc2fb1a6263d1fc5fd33844fa2e3d5947c 🚀

--- docs/printer/tokens/sidebar-items.js
+++ docs/printer/tokens/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":[["ABSURD",""],["AT",""],["CODATA",""],["CODEF",""],["COLON",""],["COMATCH",""],["COMMA",""],["DATA",""],["DEF",""],["DOT",""],["FAT_ARROW",""],["HOLE",""],["INDENT",""],["MATCH",""],["TYPE",""]]};
+window.SIDEBAR_ITEMS = {"constant":[["ABSURD",""],["AS",""],["AT",""],["CODATA",""],["CODEF",""],["COLON",""],["COMATCH",""],["COMMA",""],["DATA",""],["DEF",""],["DOT",""],["FAT_ARROW",""],["HOLE",""],["INDENT",""],["MATCH",""],["TYPE",""]]};
