678ca1f83384c741efa010c036dd8dfdd24c769a
timsueberkrueb <dev@timsueberkrueb.io>
2023-02-02 18:53:54 +0000

    Deploying to gh-pages from @ timsueberkrueb/msc-impl@f7a33f95e9940433779cedc9c23f2b77a24f4d08 🚀

--- docs/implementors/syntax/common/de_bruijn/trait.ShiftInRange.js
+++ docs/implementors/syntax/common/de_bruijn/trait.ShiftInRange.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"core":[["impl ShiftInRange for <a class=\"struct\" href=\"core/unify/struct.Eqn.html\" title=\"struct core::unify::Eqn\">Eqn</a>"]],
+"core":[["impl ShiftInRange for <a class=\"struct\" href=\"core/unify/struct.Eqn.html\" title=\"struct core::unify::Eqn\">Eqn</a>"],["impl ShiftInRange for <a class=\"struct\" href=\"core/unify/struct.Unificator.html\" title=\"struct core::unify::Unificator\">Unificator</a>"]],
 "syntax":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
