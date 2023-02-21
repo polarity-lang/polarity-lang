bf344d10e21ba1edeb3ab4a27e1acb16b689b125
timsueberkrueb <dev@timsueberkrueb.io>
2023-02-21 14:51:09 +0000

    Deploying to gh-pages from @ timsueberkrueb/msc-impl@4ad77aa0d4174c0f769016541e0a85b1dc485f58 🚀

--- docs/xfunc/cli/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"enum":[["Command",""]],"fn":[["exec",""]],"mod":[["format",""],["ignore_colors",""],["lsp",""],["run",""],["texify",""],["xfunc",""]],"struct":[["Cli",""]]};
