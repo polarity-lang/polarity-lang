eb5079bf066098ff4d0d555c9e21472b26d3732e
timsueberkrueb <dev@timsueberkrueb.io>
2023-03-09 16:46:21 +0000

    Deploying to gh-pages from @ timsueberkrueb/msc-impl@e6b813dfebfd83416826c784cbb609d7537080ab 🚀

--- docs/implementors/syntax/common/subst/trait.Substitutable.js
+++ docs/implementors/syntax/common/subst/trait.Substitutable.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"core":[["impl Substitutable&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.67.1/alloc/rc/struct.Rc.html\" title=\"struct alloc::rc::Rc\">Rc</a>&lt;Exp&lt;UST&gt;&gt;&gt; for <a class=\"struct\" href=\"core/unify/struct.Unificator.html\" title=\"struct core::unify::Unificator\">Unificator</a>"]],
+"core":[["impl Substitutable&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.68.0/alloc/rc/struct.Rc.html\" title=\"struct alloc::rc::Rc\">Rc</a>&lt;Exp&lt;UST&gt;&gt;&gt; for <a class=\"struct\" href=\"core/unify/struct.Unificator.html\" title=\"struct core::unify::Unificator\">Unificator</a>"]],
 "syntax":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
