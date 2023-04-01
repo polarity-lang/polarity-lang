403a276533b887a5da003c9a9af0218f5027f676
timsueberkrueb <dev@timsueberkrueb.io>
2023-04-01 13:52:02 +0000

    Deploying to gh-pages from @ timsueberkrueb/msc-impl@8b6440c0f3c8f177cd6de0d3048d31e00a25ce9a 🚀

--- docs/xfunc/cli/format/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"fn":[["compute_output_stream","Compute the output stream for the “fmt” subcommand. If an output filepath is specified, then that filepath is used. Otherwise, the formatted output is printed on the terminal. If the –inplace flag is specified, then the input file is overwritten."],["exec",""],["print_prg",""],["terminal_width",""]],"struct":[["Args",""]]};
